c44f693860a2f9ce8ea45f71a4e545a2c553888d
Md. Azmat Raza <azmatraza874@gmail.com>
2026-09-11 10:07:00 +0530

    Time: 8 ms (21.64%) | Memory: 18.4 MB (82.72%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -11,28 +11,26 @@ public:
 
         priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
 
-        // Har list ka first node heap mein daalo
+
         for (ListNode* node : lists) {
             if (node != NULL) {
                 pq.push(node);
             }
         }
 
-        // Dummy node
+
         ListNode* dummy = new ListNode(0);
         ListNode* current = dummy;
 
         while (!pq.empty()) {
 
-            // Sabse chhoti value wala node
+           
             ListNode* node = pq.top();
             pq.pop();
 
-            // Answer mein add
             current->next = node;
             current = current->next;
 
-            // Next node heap mein daalo
             if (node->next != NULL) {
                 pq.push(node->next);
             }
